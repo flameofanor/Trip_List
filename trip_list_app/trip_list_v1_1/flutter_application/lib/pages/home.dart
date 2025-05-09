ee3197a7af46931335e1be8cba6caee0233f61b4
flameofanor <68980551+flameofanor@users.noreply.github.com>
2025-05-09 16:47:30 -0600

    Gui - Menu bar
    - menu bar works now, displays pages
    - added fonts
    - added support for svg images

--- trip_list_app/trip_list_v1_1/flutter_application/lib/pages/home.dart
+++ trip_list_app/trip_list_v1_1/flutter_application/lib/pages/home.dart
@@ -1,23 +1,105 @@
 import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
+import 'package:flutter_svg/flutter_svg.dart';
+import "package:path/path.dart";
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'TripList v1.2',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
+      key: scaffoldKey,
+      appBar: appBar(scaffoldKey),
+      drawer: menuDrawer()
+    );
+  }
+
+  Drawer menuDrawer() {
+    return Drawer(
+      backgroundColor: Colors.green[200],
+      child: ListView(
+        
+        children: [
+          ListTile(
+            title: Text(
+              'Gear Page',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+
+              ),
+              ),
+            onTap: () {
+              
+            }
           )
-        ),
-        centerTitle: true,
-      ),
+        ],
+      )
     );
   }
 }
 
+/// App Bar for home page
+  AppBar appBar(GlobalKey<ScaffoldState> key) {
+    return AppBar(
+      title: Text(
+        'TripList',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      backgroundColor: Colors.green,
+      elevation: 0.0,
+      centerTitle: true,
+
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/settings.svg', 
+            height: 40,
+            color: Colors.black,
+            ),
+          ),
+      ),
+        // Menu Icon
+        actions: [
+          GestureDetector(
+            onTap: () {
+              key.currentState?.openDrawer();
+            },
+            child: Container(
+                      margin: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: SvgPicture.asset(
+            'assets/icons/menu.svg', 
+            height: 40,
+            color: Colors.black,
+            ),
+                      ),
+          ),
+        ]
+      
+        
+      );
+  }
 
