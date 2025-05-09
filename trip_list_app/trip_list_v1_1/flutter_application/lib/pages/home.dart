e3e696975a5750b65f052ec1c3f6a63900d89bde
flameofanor <68980551+flameofanor@users.noreply.github.com>
2025-05-09 07:59:33 -0600

    gg

--- trip_list_app/trip_list_v1_1/flutter_application/lib/pages/home.dart
+++ trip_list_app/trip_list_v1_1/flutter_application/lib/pages/home.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'TripList',
+          'TripList v1.2',
           style: TextStyle(
             color: Colors.black,
             fontSize: 18,
