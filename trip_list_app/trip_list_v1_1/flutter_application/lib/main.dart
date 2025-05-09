1313c0791075efde26a4ae3e8229b2318ae97c0c
flameofanor <68980551+flameofanor@users.noreply.github.com>
2025-05-08 18:07:52 -0600

    Started making the GUI for android! huzzah!
    much to do still

--- trip_list_app/trip_list_v1_1/flutter_application/lib/main.dart
+++ trip_list_app/trip_list_v1_1/flutter_application/lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/pages/home_page.dart';
+import 'package:flutter_application/pages/home.dart';
 import 'package:flutter_application/services/database/database.dart'; // Import your Drift database
 import 'package:provider/provider.dart'; // Add this dependency
-import '/services/user_preferences/user_preferences.dart';
 
 late final List<String> modules;
 
 void main() {
   // Create database instance
-  WidgetsFlutterBinding.ensureInitialized();
-  loadModules();
-  final database = AppDatabase();
-  
-  runApp(
-    // Provide database to entire app
+  // WidgetsFlutterBinding.ensureInitialized();
+  // loadModules();
 
+  final database = AppDatabase();
+  runApp(
     Provider.value(
       value: database,
       child: const MyApp(),
@@ -30,22 +27,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Trip List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+	  debugShowCheckedModeBanner: false,
+	  theme: ThemeData(fontFamily: "Roboto"),
       home: const HomePage(),
     );
   }
 }
 
-Future<void> loadModules() async {
-    List<String> loaded = [];
-    final userPreferences = UserPreferences();
-    await userPreferences.initialize();
-    if (userPreferences.getModules().contains("climbing")) loaded.add("climbing");
-    if (userPreferences.getModules().contains("camping")) loaded.add("camping");
-    if (userPreferences.getModules().contains("clothing")) loaded.add("clothing");
+// Future<void> loadModules() async {
+//     List<String> loaded = [];
+//     final userPreferences = UserPreferences();
+//     await userPreferences.initialize();
+//     if (userPreferences.getModules().contains("climbing")) loaded.add("climbing");
+//     if (userPreferences.getModules().contains("camping")) loaded.add("camping");
+//     if (userPreferences.getModules().contains("clothing")) loaded.add("clothing");
     
-    modules = loaded;
-}
+//     modules = loaded;
+// }
--- trip_list_app/trip_list_v1_1/flutter_application/lib/pages/gear_management_page.dart
+++ trip_list_app/trip_list_v1_1/flutter_application/lib/pages/gear_management_page.dart
@@ -1,24 +1,25 @@
-// // import 'package:flutter/material.dart';
-// // import 'package:flutter/services.dart';
-// // import 'package:flutter_application/services/database/database.dart'; // Updated import
-// // import 'package:provider/provider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_application/services/database/database.dart'; // Updated import
+// import 'package:provider/provider.dart';
 
-// // class GearManagementPage extends StatefulWidget {
-// //   const GearManagementPage({super.key});
+// class GearManagementPage extends StatefulWidget {
+//   const GearManagementPage({super.key});
 
-// //   @override
-// //   State<GearManagementPage> createState() => _GearManagementPageState();
-// // }
+//   @override
+//   State<GearManagementPage> createState() => _GearManagementPageState();
+// }
 
-// // class _GearManagementPageState extends State<GearManagementPage> {
-// //   String? gearName;
-// //   String? gearType;
-// //   int? gearQuantity; 
-// //   int? weight;
-// //   int? price;
+// class _GearManagementPageState extends State<GearManagementPage> {
+//   String? gearName;
+//   int? gearQuantity; 
+//   int? gearWeight;
 
-// //   @override
-//  // Widget build(BuildContext context) {
+//   @override
+//   Widget build()
+
+// @override
+//  Widget build(BuildContext context) {
 //     final database = Provider.of<AppDatabase>(context);
 
 //     return Scaffold(
@@ -26,8 +27,8 @@
 //         title: const Text('Gear Management'),
 //       ),
 //       // Add a StreamBuilder to display gear items reactively
-//       body: StreamBuilder<List<MasterData>>(
-//         stream: database.master,
+//       body: Lis(
+//         allData =  database.watchAllMasterGear(),
 //         builder: (context, snapshot) {
 //           if (snapshot.connectionState == ConnectionState.waiting) {
 			
@@ -45,8 +46,7 @@
 //               final item = gearItems[index];
 //               return ListTile(
 //                 title: Text(item.name),
-//                 subtitle: Text('${item.type} • Qty: ${item.quantity}'),
-//                 trailing: Text('\$${(item.price / 100).toStringAsFixed(2)}'),
+//                 subtitle: Text('${item.getName()} • Qty: ${item.getQuantity()}'),
 //                 // onTap: editGear,
 //               );
 //             },
@@ -61,25 +61,22 @@
 //     );
 //   }
 
-// //   void editGear() {
-// //     Page _editGear(BuildContext context) {
-// //       //maybe do a new page for editing gear? or like an overlay?
-// //     }
-// //   }
+//   // void editGear() {
+//   //   Page _editGear(BuildContext context) {
+//   //     //maybe do a new page for editing gear? or like an overlay?
+//   //   }
+//   // }
 
 
-// //  Widget _addGearButton(BuildContext context) {
+//   Widget _addGearButton(BuildContext context) {
 //     final database = Provider.of<AppDatabase>(context, listen: false);
     
 //     return FloatingActionButton(
 //       onPressed: () {
 //         double h = 10;
 //         gearName = null;
-//         gearType = null;
 //         gearQuantity = null; 
-//         weight = null;
-//         price = null;
-        
+//         gearWeight = null;
 //         showDialog(
 //           context: context,
 //           builder: (_) => AlertDialog(
@@ -99,25 +96,25 @@
 //                   )
 //                 ),
 //                 SizedBox(height: h),
-//                 DropdownButtonFormField<String>(
-//                   decoration: const InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     labelText: 'Gear Type',
-//                     hintText: 'Select Gear Type',
-//                   ),
-//                   value: gearType,
-//                   items: const [
-//                     DropdownMenuItem(
-//                       value: 'climbing',
-//                       child: Text('Climbing'),
-//                     ),
-//                   ],
-//                   onChanged: (value) {
-//                     setState(() {
-//                       gearType = value!;
-//                     });
-//                   },
-//                 ),
+//                 // DropdownButtonFormField<String>(
+//                 //   decoration: const InputDecoration(
+//                 //     border: OutlineInputBorder(),
+//                 //     labelText: 'Gear Type',
+//                 //     hintText: 'Select Gear Type',
+//                 //   ),
+//                   // value: gearType,
+//                   // items: const [
+//                   //   DropdownMenuItem(
+//                   //     value: 'climbing',
+//                   //     child: Text('Climbing'),
+//                   //   ),
+//                   // ],
+//                 //   onChanged: (value) {
+//                 //     setState(() {
+//                 //       gearType = value!;
+//                 //     });
+//                 //   },
+//                 // ),
 //                 SizedBox(height: h),
 //                 TextField(
 //                   decoration: const InputDecoration(
@@ -144,47 +141,27 @@
 //                     setState(() {
 //                       double? parsedValue = double.tryParse(value);
 //                       if (parsedValue != null) {
-//                         weight = (parsedValue * 10).toInt();
+//                         gearWeight = (parsedValue * 10).toInt();
 //                       } else {
-//                         weight = 0;
+//                         gearWeight = 0;
 //                       }
 //                     });
 //                   },
 //                 ),
 //                 SizedBox(height: h),
-//                 TextField(
-//                   decoration: const InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     labelText: 'Price (\$)',
-//                     hintText: 'Enter price in dollars',
-//                   ),
-//                   keyboardType: TextInputType.numberWithOptions(decimal: true),
-//                   onChanged: (value) {
-//                     setState(() {
-//                       double? parsedValue = double.tryParse(value);
-//                       if (parsedValue != null) {
-//                         price = (parsedValue * 100).toInt();
-//                       } else {
-//                         price = 0;
-//                       }
-//                     });
-//                   },
-//                 ),
 //                 SizedBox(height: h),
 //                 ElevatedButton(
 //                   onPressed: () async {
-//                     if (gearName != null && gearType != null && gearQuantity != null && weight != null && price != null) {
-//                       database.addGear(
+//                     if (gearName != null && gearQuantity != null) {
+//                       database.addGearMaster(
 //                         gearName!, 
-//                         //gearType!, 
 //                         gearQuantity!, 
-//                         //weight!, 
-//                         price!
+//                         gearWeight ?? 0,
 //                       );
 //                       Navigator.of(context).pop();
 //                     } else {
 //                       ScaffoldMessenger.of(context).showSnackBar(
-//                         const SnackBar(content: Text('Please fill in all fields')),
+//                         const SnackBar(content: Text('Please fill in all required fields')),
 //                       );
 //                     }
 //                   },
@@ -198,7 +175,7 @@
 //       child: const Icon(Icons.add),
 //     );
 //   }
-// //}
+
 
 // // Widget _editGearButton(BuildContext context) { //pop up for gear editing. make all of these for mobile app emulator though!
 // // // later add options for web and desktop if still desired)
@@ -206,166 +183,4 @@
 
 // // }
 
-
-// // import 'package:flutter/material.dart';
-// // import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-// // import 'package:flutter_application/services/database_service.dart';
-// // import 'package:flutter/services.dart';
-
-// // class GearManagementPage extends StatefulWidget {
-// //   const GearManagementPage({super.key});
-
-// //   @override
-// //   State<GearManagementPage> createState() => _GearManagementPageState();
-// // }
-
-// // class _GearManagementPageState extends State<GearManagementPage> {
-// //   final DatabaseService _databaseService = DatabaseService.instance;
-
-// //     String? gearName;
-// //     String? gearType; // type in 'climbing'
-// //     int? gearQuantity; 
-// //     int? weight; // in tenths of a gram
-// //     int? price; // in cents
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       appBar: AppBar(
-// //         title: const Text('Gear Management'),
-// //       ),
-// //       floatingActionButton: _addGearButton(),
-// //     );
-// //     }
-
-// //     Widget _addGearButton() {
-// //         return FloatingActionButton(
-// //             onPressed: () {
-// //                     double h = 10; // height in px between data entry fields
-// //                     gearName = null;
-// //                     gearType = null; // type in 'climbing'
-// //                     gearQuantity = null; 
-// //                     weight = null; // in tenths of a gram
-// //                     price = null; // in cents
-// //                 showDialog(
-// //                     context: context,
-// //                     builder: (_) => AlertDialog(
-// //                         title: const Text('Add Gear'),
-// //                         content: Column( 
-// //                             children: [
-// //                                 FloatingActionButton(
-// //                                     onPressed: () {
-// //                                         if (gearName != null && gearType != null && gearQuantity != null && weight != null && price != null) {
-// //                                             _databaseService.addMasterGear(gearName!, gearType!, gearQuantity!, weight!, price!);
-// //                                             Navigator.of(context).pop();
-// //                                         } else {
-// //                                             // Handle the case where one or more fields are empty
-// //                                             ScaffoldMessenger.of(context).showSnackBar(
-// //                                                 const SnackBar(content: Text('Please fill in all fields')),
-// //                                             );
-// //                                         }
-// //                                     }
-// //                                 ),
-// //                                 TextField(
-// //                                     onChanged: (value) {
-// //                                         setState(() {
-// //                                             gearName = value;
-// //                                         });
-// //                                     },
-// //                                     decoration: InputDecoration(
-// //                                         border: OutlineInputBorder(),
-// //                                         hintText: 'Gear Name',
-// //                                     )
-                                    
-// //                                 ),
-// //                                 SizedBox(height: h),
-// //                                 DropdownButtonFormField<String>(
-// //                                     decoration: InputDecoration(
-// //                                         border: OutlineInputBorder(),
-// //                                         labelText: 'Gear Type',
-// //                                         hintText: 'Select Gear Type',
-// //                                     ),
-// //                                     value: gearType,
-// //                                     items: const [
-// //                                         DropdownMenuItem(
-// //                                             value: 'climbing',
-// //                                             child: Text('Climbing'),
-// //                                         ),
-// //                                     ],
-// //                                     onChanged: (value) {
-// //                                         setState(() {
-// //                                             gearType = value!;
-// //                                         });
-// //                                     },
-// //                                 ),
-// //                                 SizedBox(height: h),
-// //                                 TextField(
-// //                                     decoration: InputDecoration(
-// //                                         border: OutlineInputBorder(),
-// //                                         labelText: 'qty',
-                                        
-
-// //                                     ),
-// //                                     keyboardType: TextInputType.number,
-// //                                     inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-// //                                     onChanged: (value) {
-// //                                         setState(() {
-// //                                             gearQuantity = int.tryParse(value) ?? 0;
-// //                                         });
-// //                                     },
-// //                                 ),
-// //                                 SizedBox(height: h),
-// //                                 TextField(
-// //                                     decoration: InputDecoration(
-// //                                         border: OutlineInputBorder(),
-// //                                         labelText: 'Weight (g)',
-// //                                         hintText: 'Enter weight in grams',
-
-// //                                     ),
-// //                                     keyboardType: TextInputType.numberWithOptions(decimal: true),
-                                    
-// //                                     onChanged: (value) {
-// //                                         setState(() {
-// //                                             double? parsedValue = double.tryParse(value);
-// //                                                 if (parsedValue != null) {
-// //                                                     weight = (parsedValue * 10).round();
-// //                                                 } else {
-// //                                                 weight = 0;
-// //                                                 }
-
-// //                                         });
-// //                                     },
-                                    
-// //                                 ),
-// //                                 SizedBox(height: h),
-// //                                 TextField(
-// //                                     decoration: InputDecoration(
-// //                                         border: OutlineInputBorder(),
-// //                                         labelText: 'Price (\$)',
-// //                                         hintText: 'Enter price in dollars',
-
-// //                                     ),
-// //                                     keyboardType: TextInputType.numberWithOptions(decimal: true),
-                                    
-// //                                     onChanged: (value) {
-// //                                         setState(() {
-// //                                             double? parsedValue = double.tryParse(value);
-// //                                                 if (parsedValue != null) {
-// //                                                     price = (parsedValue * 100).round();
-// //                                                 } else {
-// //                                                 price = 0;
-// //                                                 }
-
-// //                                         });
-// //                                     },
-                                    
-// //                                 )
-// //                             ],
-// //                         )
-// //                     )
-// //                 );
-// //             },
-// //             child: const Icon(Icons.add),
-// //             );
-// //     }
-// // }
+// }
